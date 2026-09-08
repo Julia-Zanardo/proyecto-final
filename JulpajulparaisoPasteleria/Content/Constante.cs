9c4c305b1cd10cfffce31192dbf6a2ff80a04856
Julia-Zanardo <juliagarciacruz2009@gmail.com>
2026-09-08 18:10:52 -0300

    Creacion de clase Constante

---
+++ JulpajulparaisoPasteleria/Content/Constante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JulpajulparaisoPasteleria.Content
+{
+    public static class Constante
+    {
+        public const int ANCHO_VIRTUAL = 1920;
+        public const int ALTO_VIRTUAL = 1080;
+        public const int ALTO_BARRA = 150;
+    }
+}
