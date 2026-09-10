75274aab92832f172f3f45f33c33c9d944ebf5c0
Julia-Zanardo <juliagarciacruz2009@gmail.com>
2026-09-10 20:45:43 -0300

    Implementacion de logica de arrastre del ticket.

--- JulpajulparaisoPasteleria/Content/Logica/DibujadorDeTicket.cs
+++ JulpajulparaisoPasteleria/Content/Logica/DibujadorDeTicket.cs
@@ -1,29 +1,59 @@
-﻿using JulpajulparaisoPasteleria.Enumeradores;
-using JulpajulparaisoPasteleria.Modelos;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using JulpajulparaisoPasteleria.Content.Enumeradores;
+using JulpajulparaisoPasteleria.Content.Controladores;
 
 namespace JulpajulparaisoPasteleria.Content.Logica
 {
     public class DibujadorDeTicket
     {
-        private Dictionary<Enum, Texture2D> iconos;
+        private Dictionary<SaborBizcochuelo, Texture2D> iconos;
         private Texture2D imagenTicket;
-
-        public void loadContent(ContentManager content)
+        private Vector2 posicionTicket = new Vector2(1450, 00);
+        public Rectangle AreaTicket { get; private set; }
+        public Rectangle AreaTicketDefaut { get; private set; }
+        public bool EstaSiendoArrastrado { get; private set; }
+        private Vector2 desplazamiento;
+        public float Escala { get; private set; } = 5;
+        public DibujadorDeTicket()
         {
-            imagenTicket = content.Load<Texture2D>("imagenes/estacionDeOrdenes/ticket");
-            iconos = new Dictionary<Enum, Texture2D>();
-            iconos.Add(SaborBizcochuelo.CarameloVainilla, content.Load<Texture2D>("imagenes/Iconos/carameloVainilla"));
+            this.AreaTicketDefaut = new Rectangle(1450, 0, 500, 500);
         }
-        public void DibujarTicket(Pedido pedido)
+        public void LoadContent(ContentManager content)
         {
-            
+            imagenTicket = content.Load<Texture2D>("imagenes/EstacionOrdenes/ticket");
+            iconos = new Dictionary<SaborBizcochuelo, Texture2D>();
+            //iconos.Add(SaborBizcochuelo.CarameloVainilla, content.Load<Texture2D>("imagenes/Iconos/carameloVainilla"));
+
+        }
+        public void Actualizar(Vector2 posicionVirtual)
+        {
+            AreaTicket = new Rectangle((int)posicionTicket.X, (int)posicionTicket.Y, (int)(imagenTicket.Width * Escala), (int)(imagenTicket.Height * Escala));
+            if (ManejoEntrada.ElementoPresionado() && AreaTicket.Contains(posicionVirtual))
+            {
+                EstaSiendoArrastrado = true;
+                desplazamiento = posicionTicket - posicionVirtual;
+            }
+
+            if (EstaSiendoArrastrado)
+            {
+                posicionTicket = posicionVirtual + desplazamiento;
+                if (ManejoEntrada.ElementoSoltado())
+                {
+                    EstaSiendoArrastrado = false;
+                }
+            }
+        }
+        public void Dibujar( SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(imagenTicket, posicionTicket, null, Color.White, 0f, Vector2.Zero, Escala, SpriteEffects.None, 1);
+        }
+        public void acomodarTicket(Rectangle posicion,float escala)
+        {
+            posicionTicket = new Vector2 (posicion.X, posicion.Y);
+            this.Escala = escala;
         }
     }
 }
