9b24d16604e7ee3fa748febfdfa30363ebc5efa3
Julia-Zanardo <juliagarciacruz2009@gmail.com>
2026-09-10 20:44:08 -0300

    Creacion de clase DetectorDeColisiones

---
+++ JulpajulparaisoPasteleria/Content/Controladores/DetectorDeColisiones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace JulpajulparaisoPasteleria.Content.Controladores
+{
+    public class DetectorDeColisiones
+    {
+        public bool DetectarColision(Rectangle rect1, Rectangle rect2)
+        {
+            return rect1.Intersects(rect2);
+        }
+    }
+}
