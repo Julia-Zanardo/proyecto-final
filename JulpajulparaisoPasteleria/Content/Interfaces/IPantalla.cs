0490b9d66f352beebb010d44ec155b51a46e7442
Julia-Zanardo <juliagarciacruz2009@gmail.com>
2026-09-11 14:42:39 -0300

    Creacion de Interfaz Ipantalla

---
+++ JulpajulparaisoPasteleria/Content/Interfaces/IPantalla.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JulpajulparaisoPasteleria.Content.Interfaces
+{
+    public interface IPantalla
+    {
+        void LoadContent(ContentManager content);
+        void Actualizar(GameTime gameTime, Vector2 posVirtual);
+        void Dibujar(SpriteBatch dibujo);
+    }
+}
