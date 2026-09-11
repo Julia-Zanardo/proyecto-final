a9ea49fb4a5d64117a22f004159f2d9c49d9f23f
Julia-Zanardo <juliagarciacruz2009@gmail.com>
2026-09-11 14:43:52 -0300

    Creacion de gestor de pantalla

---
+++ JulpajulparaisoPasteleria/Content/Controladores/GestorDePantalla.cs
@@ -0,0 +1,41 @@
+﻿using JulpajulparaisoPasteleria.Content.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JulpajulparaisoPasteleria.Content.Controladores
+{
+    public class GestorDePantalla
+    {
+        private IPantalla pantallaActual;
+        private ContentManager content;
+        public GestorDePantalla(ContentManager content)
+        {
+            this.content = content;
+        }
+        public void CambiarPantalla(IPantalla nuevaPantalla)
+        {
+            pantallaActual = nuevaPantalla;
+            pantallaActual.LoadContent(content);
+        }
+        public void LoadContent(ContentManager content)
+        {
+            pantallaActual.LoadContent(content);
+        }
+        public void Actualizar(GameTime gameTime, Vector2 posVirtual)
+        {
+            pantallaActual.Actualizar(gameTime, posVirtual);
+            
+        }
+
+        public void Dibujar(SpriteBatch dibujo)
+        {
+            pantallaActual.Dibujar(dibujo);
+        }
+    }
+}
